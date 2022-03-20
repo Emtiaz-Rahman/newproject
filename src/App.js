7c900679807102d50ec58471b2b1405c8cdeb99e
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-03-21 00:47:19 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
-const number = 5555;
+import './App.css';
+import Cosmetic from './components/Cosmetic/Cosmetic';
+
 function App() {
   return (
     <div className="App">
-      <Persion name='rubel' nayika='mousimi'></Persion>
-      <Persion></Persion>
-      <Persion></Persion>
-
-      <Friend></Friend>
-      <Friend></Friend>
+      <Cosmetic></Cosmetic>
 
     </div>
   );
 }
 
-function Persion(props) {
-  console.log(props);
-  return (
-    <div className='persion'>
-      <h1>{props.name}</h1>
-      <p>nayika: {props.nayika}</p>
-    </div>
-  )
-}
-function Friend() {
-  return (
-    <div className='persion'>
-      <h3>Name:Hablu</h3>
-      <p>Job:hadodo</p>
-    </div>
-  )
-}
 
 
 export default App;
---
+++ src/components/Cosmetic/Cosmetic.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cosmetic = () => {
+    return (
+        <div>
+            <h1>Welcome to my cosmetic store.</h1>
+        </div>
+    );
+};
+
+export default Cosmetic;
