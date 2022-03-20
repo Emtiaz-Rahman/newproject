b6bc28a6ce215c128617bcb4e19a7a2e9a8ad2e0
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-03-20 23:47:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 5555;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Persion name='rubel' nayika='mousimi'></Persion>
+      <Persion></Persion>
+      <Persion></Persion>
+
+      <Friend></Friend>
+      <Friend></Friend>
+
     </div>
   );
 }
+
+function Persion(props) {
+  console.log(props);
+  return (
+    <div className='persion'>
+      <h1>{props.name}</h1>
+      <p>nayika: {props.nayika}</p>
+    </div>
+  )
+}
+function Friend() {
+  return (
+    <div className='persion'>
+      <h3>Name:Hablu</h3>
+      <p>Job:hadodo</p>
+    </div>
+  )
+}
+
 
 export default App;
