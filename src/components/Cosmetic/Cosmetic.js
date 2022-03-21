77bfb2b0b75e40f000de78b4b85ac180d788e141
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-03-21 20:09:13 +0600

    fake data,jsongenerator

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Cosmetics from '../Cosmetics/Cosmetics';
 
 const Cosmetic = () => {
+    const [cosmetics, setCosmetics] = useState([])
+    useEffect(() => {
+        fetch(`https://elements.free.beeceptor.com/`)
+            .then(res => res.json())
+            .then(data => setCosmetics(data))
+    }, [])
     return (
         <div>
             <h1>Welcome to my cosmetic store.</h1>
+            {
+                cosmetics.map(cosmetic => <Cosmetics
+                    key={cosmetic.id}
+                    cosmetic={cosmetic}
+                ></Cosmetics>)
+            }
         </div>
     );
 };
