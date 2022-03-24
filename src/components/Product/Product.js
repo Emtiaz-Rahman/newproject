5dfb1e64001a2cc59cc76f59de1c3cce9cdd1570
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-03-24 20:15:28 +0600

    cart set complete

---
+++ src/components/Product/Product.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const { name, img, price, seller, ratings } = props.product
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Seller:{seller}</p>
+                <p>Price:{price}</p>
+                <p>Ratings{ratings}</p>
+
+            </div>
+        </div>
+    );
+};
+
+export default Product;
